508259c1e0d94878e8030621250214cce97304c5
Your Name <your@email.com>
2023-07-01 10:11:14 -0600

    Creation of repository into gitHub, this project will be my webPage

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark 
+    font-medium text-lg'
+    >
+      <Layout className='py-8 flex items-center justify-between'>
+          <span >{new Date().getFullYear()} &copy; All Rights Reserverd.</span>
+          <div className='flex items-center'>
+          <span className='text-primary text-2xl px-1'>&#9768;</span>
+          by&nbsp;<Link href="/" className='underline
+          underline-offset-2'>Fabian Morales</Link>
+          </div>
+          
+          <Link href="/">Say Hello</Link>
+      </Layout>
+    </footer>
+    
+  )
+}
+
+export default Footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import Logo from "./logo";
+import { GithubIcon, LinkedInIcon, MoonIcon, SunIcon } from "./Icons";
+import {motion} from "framer-motion"
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[3px] inline-block bg-dark
+      absolute left-0 -bottom-0.5
+      group-hover:w-full transition-[width] ease duration-300
+      ${router.asPath === href ? "w-full" : "w-0"}`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <header className="w-full px-32 py-8  font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+
+      <nav className="flex items-center  justify-center flex-wrap-rev ">
+        <motion.a href="https://github.com/femorales7" target={"_blank"}
+        whileHover={{y:-2}}
+        className="w-6 mr-3"
+        whileTap={{scale:0.9}}>
+          <GithubIcon/>
+        </motion.a>
+        <motion.a href="https://www.linkedin.com/in/fabian-morales-782b02145" target={"_blank"}
+        whileHover={{y:-2}}
+        className="w-6 mr-3"
+        whileTap={{scale:0.9}}>
+          <LinkedInIcon/>
+        </motion.a>
+        <motion.a href="/" target={"_blank"}
+        whileHover={{y:-2}}
+        className="w-6 mr-3"
+        whileTap={{scale:0.9}}>
+          <MoonIcon/>
+        </motion.a>
+        
+        <motion.a href="/" target={"_blank"}
+        whileHover={{y:-2}}
+        className="w-6 mr-3"
+        whileTap={{scale:0.9}}>
+          <SunIcon/>
+        </motion.a>
+        
+      </nav>
+      <div className="absolute left-[50%] top-2  traslate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
