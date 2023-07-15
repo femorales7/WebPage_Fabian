2bcff85a44a7c308cf0fd6299110ba42df4afa98
Your Name <your@email.com>
2023-07-14 20:36:35 -0600

    change the resume download

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
               </p>
               <div className="flex items-cener self-start mt-2 lg:self-center">
                 <Link
-                  href="/Fabian_Morales_Resume_02-06-2023-01-04-26.pdf"
+                  href="/Fabian_Morales_Resume_05-07-2023-16-37-36.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark 
