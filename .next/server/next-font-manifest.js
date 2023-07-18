3fd24f84bb8222c554ae5d70bdf7ab591c312d8b
Your Name <your@email.com>
2023-07-18 16:06:30 -0600

    change favicon.ico

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,6 +1,10 @@
 self.__NEXT_FONT_MANIFEST={
-  "pages": {},
+  "pages": {
+    "/_app": [
+      "static/media/1060bab20f18b5c2-s.p.woff2"
+    ]
+  },
   "app": {},
   "appUsingSizeAdjust": false,
-  "pagesUsingSizeAdjust": false
+  "pagesUsingSizeAdjust": true
 }
