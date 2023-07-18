f6b1a0610ae948bbdbd1e6281b612c999a503491
Your Name <your@email.com>
2023-07-18 16:08:57 -0600

    change favicon.ico

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/_next/:path*',
-        destination: `/_next/:path*`,
-      },
-      {
-        source: '/favicon.ico',
-        destination: '/favicon.ico',
-      },
-      {
-        source: '/vercel.svg',
-        destination: '/vercel.svg',
-      },
-      // Add this configuration to redirect the title
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-        permanent: true,
-        query: { title: 'Fabian Morales' },
-      },
-    ];
-  },
-};
+}
+
 module.exports = nextConfig
